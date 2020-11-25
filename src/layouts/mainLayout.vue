dc0cd1b8a583b78ec38ea35d4cceddd2b088a952
coelilumenus <74508333+coelilumenus@users.noreply.github.com>
2020-11-25 05:54:21 +0300

    need to made filter for date with vue3, its a part 4-15 min

---
+++ src/layouts/mainLayout.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="app-main-layout">
+    <navbar @click="isOpen = !isOpen" />
+    <sidebar v-model="isOpen" />
+    <main class="app-content" :class="{full: !isOpen}">
+      <div class="app-page">
+        <router-view />
+      </div>
+    </main>
+    <div class="fixed-action-btn">
+      <router-link class="btn-floating btn-large blue" to="/record">
+        <i class="large material-icons">add</i>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from '../components/app/navbar'
+import Sidebar from '../components/app/sidebar'
+export default {
+  name: 'main-layout',
+  data: () => ({
+    isOpen: true
+  }),
+  components: {
+    Navbar, Sidebar
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,62 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/record',
+    name: 'record',
+    meta: { layout: 'main' },
+    component: () => import('../views/record.vue')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/profile',
+    name: 'profile',
+    meta: { layout: 'main' },
+    component: () => import('../views/profile.vue')
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: { layout: 'main' },
+    component: () => import('../views/planning.vue')
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: { layout: 'main' },
+    component: () => import('../views/history.vue')
+  },
+  {
+    path: '/detail-record',
+    name: 'detail-record',
+    meta: { layout: 'main' },
+    component: () => import('../views/detail-record.vue')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    meta: { layout: 'empty' },
+    component: () => import('../views/register.vue')
+  },
+  {
+    path: '/',
+    name: 'home',
+    meta: { layout: 'main' },
+    component: () => import('../views/home.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    meta: { layout: 'empty' },
+    component: () => import('../views/login.vue')
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: { layout: 'main' },
+    component: () => import('../views/categories.vue')
   }
 ]
 
