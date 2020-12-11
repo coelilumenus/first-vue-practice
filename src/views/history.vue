76326e96aa03dae96fd3e93587b922788694f756
coelilumenus <74508333+coelilumenus@users.noreply.github.com>
2020-12-11 03:45:55 +0300

    add history but need to made paging and other

--- src/views/history.vue
+++ src/views/history.vue
@@ -7,37 +7,41 @@
     <div class="history-chart">
       <canvas></canvas>
     </div>
-
-    <section>
-      <table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Сумма</th>
-            <th>Дата</th>
-            <th>Категория</th>
-            <th>Тип</th>
-            <th>Открыть</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>1212</td>
-            <td>12.12.32</td>
-            <td>name</td>
-            <td>
-              <span class="white-text badge red">Расход</span>
-            </td>
-            <td>
-              <button class="btn-small btn">
-                <i class="material-icons">open_in_new</i>
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+    <loader v-if="loading" />
+    <p class="center" v-else-if="!records.length">Записей пока нет. <router-link to="/record">Создать новую запись</router-link></p>
+    <section v-else>
+      <historyTable :records="records" />
     </section>
   </div>
 </template>
+
+<script>
+import historyTable from '@/components/historyTable'
+import loader from '../components/app/loader.vue'
+export default {
+  name: 'history',
+  data: () => ({
+    loading: true,
+    recrods: [],
+    categories: []
+  }),
+  async mounted () {
+    // this.records = await this.$store.dispatch('fetchRecords')
+    const records = await this.$store.dispatch('fetchRecords')
+    const categories = await this.$store.dispatch('fetchCategories')
+    this.records = records.map(record => {
+      return {
+        ...record,
+        categoryName: categories.find(c => c.id === record.categoryId).title,
+        typeClass: record.type === 'income' ? 'green' : 'red',
+        typeText: record.type === 'income' ? 'Доход' : 'Расход'
+      }
+    })
+    this.loading = false
+  },
+  components: {
+    historyTable,
+    loader
+  }
+}
+</script>
